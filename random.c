cf4eda141a38286a80b99be8211779647849849c
lixintyw <2630693297@qq.com>
2017-07-10 21:56:55 +0800

    “三个人随机选一个”

--- random.c
+++ random.c
@@ -3,9 +3,19 @@
 #include<time.h>
 int main()
 {   
-	int i;	
+	int i,b,a;	
 	srand(time(NULL));
 	for(i=0;i<10;i++)
-	printf("%5d\n",rand()%101);
+	a=rand();
+	b=(a%3);
+	switch (b)
+	{
+		case 0:
+		printf("余杰\n");break;
+		case 1:	
+		printf("三胖\n");break;
+		default:
+		printf("山炮\n");break;
+	}	
 	return 0;
 }
